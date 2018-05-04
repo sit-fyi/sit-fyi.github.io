a3a9ad3182776612fd0873f54c0fd79e3cd30d30
Yurii Rashkovskii <yrashk@gmail.com>
2018-05-04 00:25:48 -0700

    Problem: not trivial to install sit for newcomers
    This process involves finding the right binaries,
    putting them somewhere, making them executable
    and adding them to PATH.
    
    Solution: provide a one-liner installation
    
    curl http://sit-it.org/install.sh | sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+repo=sit-it/sit
+get_latest_release() {
+        curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+latest_release=$(get_latest_release "${repo}")
+os=$(uname)
+arch=$(uname -m)
+install=$HOME/.sit
+
+if [ "${os}" == "Linux" ]; then
+        os=linux
+fi        
+
+if [ "${os}" == "Dawin" ]; then
+        os=macosx
+fi        
+
+mkdir -p ${install}
+
+echo Installing SIT for $(whoami)
+echo Latest available release: ${latest_release}
+echo Downloading sit
+curl -# -L "https://github.com/sit-it/sit/releases/download/${latest_release}/sit-${arch}-${os}" -o "${install}/sit"
+echo Downloading sit-web
+curl -# -L "https://github.com/sit-it/sit/releases/download/${latest_release}/sit-web-${arch}-${os}" -o "${install}/sit-web"
+chmod +x ${install}/sit ${install}/sit-web
+
+echo
+echo Execute and add the following command to your shell startup script:
+echo
+echo export PATH=${install}:\$PATH
+
